51b34c04857b1a4924846813798b0eece9a257d9
arsene <gustoybrofis@gmail.com>
2025-01-30 19:59:13 +0500

    Миграции

---
+++ test/Migrations/20250130145546_Initial.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace test.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    IdNumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RepairTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Cost = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepairTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    IdNumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CarNumber = table.Column<string>(type: "text", nullable: true),
+                    Manufacturer = table.Column<string>(type: "text", nullable: true),
+                    Model = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vehicles_Clients_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Repairs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ShopId = table.Column<Guid>(type: "uuid", nullable: true),
+                    VehicleId = table.Column<Guid>(type: "uuid", nullable: true),
+                    RepairTypeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Cost = table.Column<float>(type: "real", nullable: false),
+                    RepairStatus = table.Column<int>(type: "integer", nullable: false),
+                    FinishedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Mileage = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repairs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Repairs_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Repairs_RepairTypes_RepairTypeId",
+                        column: x => x.RepairTypeId,
+                        principalTable: "RepairTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Repairs_Services_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Repairs_Vehicles_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "RepairTypes",
+                columns: new[] { "Id", "Cost", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("6008ed75-fda2-4268-b9cf-06a29b71d879"), 5000f, "Выправление вмятин" },
+                    { new Guid("dfac210a-e576-4c1c-9e08-45a48d003d1d"), 8000f, "Замена масла" },
+                    { new Guid("84c3967b-4420-4d17-b23c-6fac77b06b4a"), 500000f, "Замена двигателя" },
+                    { new Guid("80ede21c-0607-466c-adc4-998bfb2fc0ce"), 2500f, "Замена колес/покрышек" },
+                    { new Guid("83a83423-f1b4-424d-b047-5eb0ab2a272b"), 800f, "Установка радиоприемника" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repairs_ClientId",
+                table: "Repairs",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repairs_RepairTypeId",
+                table: "Repairs",
+                column: "RepairTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repairs_ShopId",
+                table: "Repairs",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repairs_VehicleId",
+                table: "Repairs",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_OwnerId",
+                table: "Vehicles",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Repairs");
+
+            migrationBuilder.DropTable(
+                name: "RepairTypes");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
