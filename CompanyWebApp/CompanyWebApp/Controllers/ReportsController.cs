27c7e2c06f30c672683c9d11fb39934a05d7234c
arsene <gustoybrofis@gmail.com>
2025-02-02 21:56:26 +0500

    Веб-интерфейс

---
+++ CompanyWebApp/CompanyWebApp/Controllers/ReportsController.cs
@@ -0,0 +1,39 @@
+﻿using CompanyWebApp.Models;
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace CompanyWebApp.Controllers
+{
+	public class ReportsController : Controller
+	{
+		private readonly HttpClient _httpClient;
+
+		public ReportsController(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		private async Task<IEnumerable<Report>?> GetReports()
+		{
+			var reports = await _httpClient.GetAsync("https://localhost:5001/company/client");
+
+			if (reports.IsSuccessStatusCode)
+			{
+				return await reports.Content.ReadFromJsonAsync<IEnumerable<Report>>();
+			}
+
+			return null;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var reports = await GetReports();
+			return View("ReportsView", reports);
+		}
+	}
+}
+
---
+++ CompanyWebApp/CompanyWebApp/Models/Report.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CompanyWebApp.Models
+{
+    public class Report
+    {
+        public string Name { get; set; }
+        public float Cost { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
