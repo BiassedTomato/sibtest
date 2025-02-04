5b26d0cea74cad49af5014c7eb85996dc32efba6
arsene <gustoybrofis@gmail.com>
2025-02-04 15:38:48 +0500

    Вьюшки для отчетов

--- CompanyWebApp/CompanyWebApp/Controllers/ReportsController.cs
+++ CompanyWebApp/CompanyWebApp/Controllers/ReportsController.cs
@@ -5,9 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Net.Http.Json;
+using CompanyWebApp.Models.ReportData;
+using System.Web;
 
 namespace CompanyWebApp.Controllers
 {
+	[Route("[controller]")]
 	public class ReportsController : Controller
 	{
 		private readonly HttpClient _httpClient;
@@ -17,13 +20,84 @@ namespace CompanyWebApp.Controllers
 			_httpClient = httpClient;
 		}
 
-		private async Task<IEnumerable<Report>?> GetReports()
+		private async Task<VehicleReport?> GetVehicleReport(string vehicleNumber, string startDate, string endDate)
 		{
-			var reports = await _httpClient.GetAsync("https://localhost:5001/company/client");
+			var builder = new UriBuilder("https://localhost:5001/company/vehicle");
+			var query = HttpUtility.ParseQueryString(builder.Query);
+
+			query["idNumber"] = vehicleNumber;
+			query["startDate"] = startDate;
+			query["endDate"] = endDate;
+
+			builder.Query = query.ToString();
+
+			var reports = await _httpClient.GetAsync(builder.ToString());
 
 			if (reports.IsSuccessStatusCode)
 			{
-				return await reports.Content.ReadFromJsonAsync<IEnumerable<Report>>();
+				return await reports.Content.ReadFromJsonAsync<VehicleReport>();
+			}
+
+			return null;
+		}
+
+		private async Task<ShopReport?> GetShopReport(string shopNumber, string startDate, string endDate)
+		{
+			var builder = new UriBuilder("https://localhost:5001/company/shop");
+			var query = HttpUtility.ParseQueryString(builder.Query);
+
+			query["idNumber"] = shopNumber;
+			query["startDate"] = startDate;
+			query["endDate"] = endDate;
+
+			builder.Query = query.ToString();
+
+			var reports = await _httpClient.GetAsync(builder.ToString());
+
+			if (reports.IsSuccessStatusCode)
+			{
+				return await reports.Content.ReadFromJsonAsync<ShopReport>();
+			}
+
+			return null;
+		}
+
+		private async Task<ClientReport?> GetClientReport(string clientNumber, string startDate, string endDate)
+		{
+			var builder = new UriBuilder("https://localhost:5001/company/client");
+			var query = HttpUtility.ParseQueryString(builder.Query);
+
+			query["idNumber"] = clientNumber;
+			query["startDate"] = startDate;
+			query["endDate"] = endDate;
+
+			builder.Query = query.ToString();
+
+			var reports = await _httpClient.GetAsync(builder.ToString());
+
+			if (reports.IsSuccessStatusCode)
+			{
+				return await reports.Content.ReadFromJsonAsync<ClientReport>();
+			}
+
+			return null;
+		}
+
+		private async Task<RepairsReport?> GetRepairsReport(string startDate, string endDate)
+		{
+			var builder = new UriBuilder("https://localhost:5001/company/repairs");
+			var query = HttpUtility.ParseQueryString(builder.Query);
+
+			query["startDate"] = startDate;
+			query["endDate"] = endDate;
+
+			builder.Query = query.ToString();
+
+			var reports = await _httpClient.GetAsync(builder.ToString());
+
+			if (reports.IsSuccessStatusCode)
+			{
+				return await reports.Content.ReadFromJsonAsync<RepairsReport>();
 			}
 
 			return null;
@@ -31,8 +105,31 @@ namespace CompanyWebApp.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			var reports = await GetReports();
-			return View("ReportsView", reports);
+			return View();
+		}
+
+		[Route("shop")]
+		public async Task<IActionResult> ShopReport([FromQuery]string shopNumber, string startDate, string endDate)
+		{
+			return View("ShopReportView", await GetShopReport(shopNumber, startDate, endDate));
+		}
+
+		[Route("client")]
+		public async Task<IActionResult> ClientReport([FromQuery] string clientNumber, string startDate, string endDate)
+		{
+			return View("ClientReportView", await GetClientReport(clientNumber, startDate, endDate));
+		}
+
+		[Route("repairs")]
+		public async Task<IActionResult> RepairsReport([FromQuery] string startDate, string endDate)
+		{
+			return View("RepairsReportView", await GetRepairsReport(startDate, endDate));
+		}
+
+		[Route("vehicle")]
+		public async Task<IActionResult> VehicleReport([FromQuery] string vehicleNumber, string startDate, string endDate)
+		{
+			return View("VehicleReportView", await GetVehicleReport(vehicleNumber, startDate, endDate));
 		}
 	}
 }
---
+++ CompanyWebApp/CompanyWebApp/Models/ReportData/ShopReport.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyWebApp.Models.ReportData
+{
+	public class ShopReport
+	{
+		public string ShopId { get; set; }
+		public string ShopName { get; set; }
+		public IEnumerable<RepairReportDTO> Repairs { get; set; }
+		public int TotalCount { get; set; }
+		public float TotalSum { get; set; }
+	}
+
+	public class VehicleReport
+	{
+		public string VehicleNumber { get; set; }
+		public string VehicleModel { get; set; }
+		public IEnumerable<RepairReportDTO> Repairs { get; set; }
+		public int TotalCount { get; set; }
+		public float TotalSum { get; set; }
+		public float MileageDelta { get; set; }
+	}
+
+	public class ClientReport
+	{
+		public string ClientId { get; set; }
+		public string ClientName { get; set; }
+		public IEnumerable<RepairReportDTO> Repairs { get; set; }
+		public int TotalCount { get; set; }
+		public float TotalSum { get; set; }
+	}
+
+	public class RepairsReport
+	{
+		public IEnumerable<RepairReportDTO> Repairs { get; set; }
+		public int TotalCount { get; set; }
+		public float TotalSum { get; set; }
+	}
+
+	public class RepairReportDTO
+	{
+		public string ClientName { get; set; }
+		public string ClientId { get; set; }
+		public string ServiceType { get; set; }
+		public string ShopName { get; set; }
+		public string ShopId { get; set; }
+		public float Cost { get; set; }
+		public string VehicleModel { get; set; }
+		public string VehicleMileage { get; set; }
+		public string VehicleNumber { get; set; }
+		public DateTime FinishedDate { get; set; }
+	}
+}
